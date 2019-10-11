1a61dfb7b3307816e33c13227eb2c718c6b9510f
SylteMann <kristianbsylte@gmail.com>
2019-10-11 18:05:51 +0200

    har muligens ferdigstilt logikken, jobber nå med å rendre innholdet

--- P3/djikstraTest/script.js
+++ P3/djikstraTest/script.js
@@ -53,18 +53,76 @@ class DGrid {
         this.copy();
     }
 
-    copy() {
-        console.log(this.initialGrid);
-        for (let i = 0; i < this.initialGrid.length; i++) {
+    render() {
+        document.body.innerHTML = "";
+        for (let i = 0; i < this.dGrid.length; i++) {
             console.log("test");
+            let elem = document.createElement("div");
+            elem.className = "node";
+            let elemInner = document.createElement("div");
+            elem.style.top = this.dGrid[i].y + "px";
+            elem.style.left = this.dGrid[i].x + "px";
+            elemInner.innerHTML = "id: " + i;
+            elemInner.innerHTML += "<br>";
+            elemInner.innerHTML += "od: " + this.dGrid[i].distanceFromOrigin;
+            elemInner.innerHTML += "<br>x: " + this.dGrid[i].x + ", y: " + this.dGrid[i].y;
+            elem.appendChild(elemInner);
+            document.body.appendChild(elem);
+        }
+        this.renderAttachments();
+    }
+
+    renderAttachments() {
+        let getSmallest = function (a, b) {
+            if (a < b) {
+                return a;
+            } else {
+                return b;
+            }
+        }
+        for (let i = 0; i < this.dGrid.length; i++) {
+            let currentNode = this.dGrid[i];
+            for (let j = 0; j < currentNode.attachments.length; j++) {
+                let length = currentNode.attachments[j].distance;
+                let elem = document.createElement("div");
+
+                let destNodeX = this.dGrid[currentNode.attachments[j].id].x;
+                let destNodeY = this.dGrid[currentNode.attachments[j].id].y;
+
+                let dx = Math.abs(currentNode.x - destNodeX);
+                let dy = Math.abs(currentNode.y - destNodeY);
+                let cx = getSmallest(currentNode.x, destNodeX) + dx / 2;
+                let cy = getSmallest(currentNode.y, destNodeY) + dy / 2;
+
+                console.log(this.dGrid[currentNode.attachments[j].id].x);
+
+                elem.style.width = length + "px";
+
+                let rotation = -Math.acos(dx / length) / (2 * Math.PI) * 360;
+
+                // elem.style.transform = "rotate(" + rotation + "deg)";
+                //elem.style.marginLeft = dx / 2;
+                //elem.style.marginTop = dy / 2;
+
+                elem.style.left = currentNode.x + 25 + (length / 2 * Math.cos(rotation / 360 * 2 * Math.PI)) + "px";
+                elem.style.top = currentNode.y + 25 + "px";
+                elem.style.transform = "rotate(" + rotation + "deg)";
+
+                elem.className = "line";
+
+                document.body.appendChild(elem);
+                console.log("Origin:", i, "Tilknytning:", currentNode.attachments[j], "element:", elem);
+            }
+        }
+    }
+
+    copy() {
+        for (let i = 0; i < this.initialGrid.length; i++) {
             let dNode = new DNode(this.initialGrid[i].x, this.initialGrid[i].y);
             this.dGrid.push(dNode);
         }
         for (let i = 0; i < this.initialGrid.length; i++) {
-            console.log("innerTest");
-            //let cNode = this.dGrid[i];
             for (let j = 0; j < this.initialGrid[i].attachments.length; j++) {
-                console.log(this.initialGrid[i].attachments[j]);
                 this.attach(i, this.initialGrid[i].attachments[j].nodeId, this.initialGrid[i].attachments[j].distance);
 
             }
@@ -75,14 +133,12 @@ class DGrid {
         //Sjekker om tilknytningen allerede finnes
         for (let i = 0; i < this.dGrid[id1].attachments.length; i++) {
             if (this.dGrid[id1].attachments[i].id == id2) {
-                console.log("duplisert funnet")
                 return false;
             }
         }
 
         for (let i = 0; i < this.dGrid[id2].attachments.length; i++) {
             if (this.dGrid[id2].attachments[i].id == id2) {
-                console.log("duplisert funnet")
                 return false;
             }
         }
@@ -96,8 +152,6 @@ class DGrid {
             let id2y = this.grid[id2].y;
             let dist = Math.sqrt(Math.pow((id1x - id2x), 2) + Math.pow((id1y - id2y), 2));
 
-            console.log(dist)
-
 
             this.dGrid[id1].addAttachment(id2, dist);
             this.dGrid[id2].addAttachment(id1, dist);
@@ -108,11 +162,18 @@ class DGrid {
         this.dGrid[id2].addAttachment(id1, dist);
     }
 
+    setOrigin(id) {
+        this.dGrid[id].distanceFromOrigin = 0;
+        this.dGrid[id].shortestOrigin = id;
+        this.check(id, id);
+        this.render();
+    }
+
     check(origin, nodeId) {
         let currentNode = this.dGrid[nodeId];
         for (let i = 0; i < currentNode.attachments.length; i++) {
-            if (!currentNode[i].visited) {
-                this.visit(origin, currentNode.attachments[i].id, currentNode.attachments[i].dist);
+            if (!(currentNode.attachments[i].visited)) {
+                this.visit(origin, currentNode.attachments[i].id, currentNode.attachments[i].distance);
                 currentNode.visited = true;
             }
         }
@@ -122,12 +183,8 @@ class DGrid {
         let total = this.dGrid[originId].distanceFromOrigin + dist;
         if (this.dGrid[targetId].distanceFromOrigin > total) {
             this.dGrid[targetId].setDistance(total);
-            this.dGrid.shortestOrigin = originId;
+            this.dGrid[targetId].shortestOrigin = originId;
         }
-    }
-
-    addNode(x, y) {
-
     }
 
     chooseOrigin(id) {
@@ -168,4 +225,7 @@ class DAttachment {
 
 var grid = new Grid;
 grid.attach(grid.addNode(134, 523), grid.addNode(200, 100));
+grid.attach(grid.addNode(421, 253), grid.addNode(224, 575));
+grid.attach(1, 3);
 var djGrid = new DGrid(grid);
+djGrid.setOrigin(0);
