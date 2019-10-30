d78a72f7f335942e6ff7ef01e8955a59861a360e
Kimo <chaim0314@gmail.com>
2019-10-30 16:02:30 +0100

    begynte på artikkelloadingsfunksjoner

--- P3/scripts/artikkel.js
+++ P3/scripts/artikkel.js
@@ -1,7 +1,7 @@
 const artikkel = [{
-    artikkelTitel: 'Absultaket',
-    artikkelTekst: 'afafafaafa',
-    artikkelBilde: 'img'
+    artikkelTitel: 'Lorem Ipsum',
+    artikkelTekst: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+    artikkelBilde: 'img/vaffelmeme.jpg'
     },
                 {
     artikkelTitel: 'Absultaket',
@@ -20,14 +20,14 @@ const artikkel = [{
     }
 ];
 
-for (i in artikkel) {
-    let div = document.createElement("div");
-    div.innerText = artikkel[i].artikkelTitel;
+// for (i in artikkel) {
+//     let div = document.createElement("div");
+//     div.innerText = artikkel[i].artikkelTitel;
     
-    console.log(artikkel[i].artikkelTitel);
-    console.log(artikkel[i].artikkelTekst);
-    console.log("--------------------");
-}
+//     console.log(artikkel[i].artikkelTitel);
+//     console.log(artikkel[i].artikkelTekst);
+//     console.log("--------------------");
+// }
 
 function artikkelBytte(evt){
     event.preventDefault();
@@ -38,3 +38,27 @@ function artikkelBytte(evt){
     tekst.innerHTML = artikkel[evt].artikkelTekst;
     bilde.src = artikkel[evt].artikkelBilde;
 }
+
+function makeElement(tag, klass) {
+    elem = document.createElement(tag);
+    elem.setAttribute("class", klass);
+    return elem;
+}
+
+function createArtikkel(parent, artObj) {
+    console.log(makeElement("div","article_home"))
+
+}
+
+function artikkelHome() {
+    for (let i = 0; i < 3; i++) {
+        createArtikkel(news, artikkel[i])
+    }
+    console.log("Hello")
+}
+
+
+let news = document.getElementById("home_news");
+news.onload = artikkelHome()
+div = document.createElement("div")
+console.log(div)
