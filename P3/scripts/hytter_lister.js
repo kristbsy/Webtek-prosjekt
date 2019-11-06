6c452931ff20cf9b400df55f6eda2fc34e5382ba
Leo Gimre <leogimre@hotmail.no>
2019-11-06 20:41:30 +0100

    god kode

---
+++ P3/scripts/hytter_lister.js
@@ -0,0 +1,27 @@
+function byttHytte(evt){
+  let info = document.getElementById("infoDiv");
+  let navn = document.getElementById("hytteNavn")
+  let tilbehor = document.getElementById("tilbehørDiv");
+  navn.innerHTML = hytter[evt].info.name;
+  info.innerHTML =  hytter[evt].info.longText;
+  tilbehor.innerHTML =  hytter[evt].info.features;
+  slideshow_hytte_generator(evt);
+}
+
+
+function MakeList(evt) { //Function to make the listed elemetns show in the correct order.
+let list = document.getElementById("hytter_liste")
+for (let i = 0; i < hytter.length; i++) {
+  let list = document.getElementById("hytter_liste")
+  let node = document.createElement("li");
+
+  node.id = 'hytteNr' + i;
+  node.setAttribute('onclick', 'byttHytte(' + i + ')')
+
+  let textnode = document.createTextNode(hytter[i].info.name); //Defines what will be writen in one of the other elements.
+  node.appendChild(textnode); //Inserts the text into the li element.
+  list.appendChild(node);
+}
+}
+
+MakeList()
--- P3/scripts/slideshow.js
+++ P3/scripts/slideshow.js
@@ -36,226 +36,11 @@ let slideshow_hovedside = [{
   }
 
 ]
-//liste med liste med object
-
-let slideshow_hytte = [
-  [ // 0
-    {
-      kilde: "../img/hyttebilder/absultaket/absultaket_ovn.jpg",
-      alternativ_tekst: "Bilde av Absultekets ovn"
-    },
-    {
-      kilde: "../img/hyttebilder/absultaket/absultaket_stue.jpg",
-      alternativ_tekst: "Bilde av Absultekets stue"
-    },
-    {
-      kilde: "../img/hyttebilder/absultaket/absultaket_ute.jpg",
-      alternativ_tekst: "Bilde av Abstultekets uteområde"
-    },
-    {
-      kilde: "../img/hyttebilder/absultaket/absultaket.jpg",
-      alternativ_tekst: "Bilde av hytten Absultaket"
-    }
-  ],
-
-  [ // 1
-    {
-      kilde: "../img/hyttebilder/bjørnehiet/bjørnehiet_kjokken.jpg",
-      alternativ_tekst: "Bilde av Bjørnehiets kjøkken"
-    },
-    {
-      kilde: "../img/hyttebilder/bjørnehiet/bjørnehiet_peis.jpg",
-      alternativ_tekst: "Bilde av Bjørnehiets peis"
-    },
-    {
-      kilde: "../img/hyttebilder/bjørnehiet/bjørnehiet_stue.jpg",
-      alternativ_tekst: "Bilde av Bjørnehiets stue"
-    },
-    {
-      kilde: "../img/hyttebilder/bjørnehiet/bjørnehiet.jpg",
-      alternativ_tekst: "Bilde av hytten Bjørnehiet"
-    }
-  ],
-
-  [ // 2
-    {
-      kilde: "../img/hyttebilder/einhaugen/einhaugen_ute.jpg",
-      alternativ_tekst: "Bilde av Einhaugens uteområde"
-    },
-    {
-      kilde: "../img/hyttebilder/einhaugen/einhaugen_sov.jpg",
-      alternativ_tekst: "Bilde av Einhaugens soverom"
-    },
-    {
-      kilde: "../img/hyttebilder/einhaugen/einhaugen_inne.jpg",
-      alternativ_tekst: "Bilde av Einhaugens innteriør"
-    },
-    {
-      kilde: "../img/hyttebilder/einhaugen/einhaugen.jpg",
-      alternativ_tekst: "Bilde av hytten Einhaugens"
-    }
-  ],
-
-  [ // 3
-    {
-      kilde: "../img/hyttebilder/elvskinnet/elvskinnet_stue.jpg",
-      alternativ_tekst: "Bilde av Elvskinnets stue"
-    },
-    {
-      kilde: "../img/hyttebilder/elvskinnet/elvskinnet_peis.jpg",
-      alternativ_tekst: "Bilde av Elvskinnets peis"
-    },
-    {
-      kilde: "../img/hyttebilder/elvskinnet/elvskinnet_kjokken.jpg",
-      alternativ_tekst: "Bilde av Elvskinnets kjøkken"
-    },
-    {
-      kilde: "../img/hyttebilder/elvskinnet/elvskinnet.jpg",
-      alternativ_tekst: "Bilde av hytten Elvskinnet"
-    }
-  ],
-
-  [ // 4
-    {
-      kilde: "../img/hyttebilder/flåbånnhytta/flåbånnhytta_stue.jpg",
-      alternativ_tekst: "Bilde av Flåbånnhyttas stue"
-    },
-    {
-      kilde: "../img/hyttebilder/flåbånnhytta/flåbånnhytta_sov.jpg",
-      alternativ_tekst: "Bilde av Flåbånnhyttas soverom"
-    },
-    {
-      kilde: "../img/hyttebilder/flåbånnhytta/flåbånnhytta_kjokken.jpg",
-      alternativ_tekst: "Bilde av Flåbånnhyttas kjøkken"
-    },
-    {
-      kilde: "../img/hyttebilder/flåbånnhytta/flåbånnhytta.jpg",
-      alternativ_tekst: "Bilde av hytten Flåbånnhytta"
-    }
-  ],
-
-  [ // 5
-    {
-      kilde: "../img/hyttebilder/gaustadhytta/gaustadhytta_ved.jpg",
-      alternativ_tekst: "Bilde av Gaustadhytta vedmuligheter"
-    },
-    {
-      kilde: "../img/hyttebilder/gaustadhytta/gaustadhytta_ute.jpg",
-      alternativ_tekst: "Bilde av Gaustadhyttas uteområde"
-    },
-    {
-      kilde: "../img/hyttebilder/gaustadhytta/gaustadhytta_sov.jpg",
-      alternativ_tekst: "Bilde av Gaustadhyttas soverom"
-    },
-    {
-      kilde: "../img/hyttebilder/gaustadhytta/gaustadhytta.jpg",
-      alternativ_tekst: "Bilde av hytten Gaustadhytta"
-    }
-  ],
-
-  [ // 6
-    {
-      kilde: "../img/hyttebilder/høtta/høtta_stue.jpg",
-      alternativ_tekst: "Bilde av Høttas stue"
-    },
-    {
-      kilde: "../img/hyttebilder/høtta/høtta_sov.jpg",
-      alternativ_tekst: "Bilde av Høttas soverom"
-    },
-    {
-      kilde: "../img/hyttebilder/høtta/høtta_peis.jpg",
-      alternativ_tekst: "Bilde av Høttas peis"
-    },
-    {
-      kilde: "../img/hyttebilder/høtta/høtta.jpg",
-      alternativ_tekst: "Bilde av hytten Høtta"
-    }
-  ],
-
-  [ // 7
-    {
-      kilde: "../img/hyttebilder/nuddustruddu/nuddustruddu_sov.jpg",
-      alternativ_tekst: "Bilde av Nuddustruddus soverom"
-    },
-    {
-      kilde: "../img/hyttebilder/nuddustruddu/nuddustruddu_stue.jpg",
-      alternativ_tekst: "Bilde av Nuddustruddus stue"
-    },
-    {
-      kilde: "../img/hyttebilder/nuddustruddu/nuddustruddu_ute.jpg",
-      alternativ_tekst: "Bilde av Nuddustruddus uteområde"
-    },
-    {
-      kilde: "../img/hyttebilder/nuddustruddu/nuddustruddu.jpg",
-      alternativ_tekst: "Bilde av hytten Nuddustruddu"
-    }
-  ],
-
-  [ // 8
-    {
-      kilde: "../img/hyttebilder/puudklubb/puudklubb_stue.jpg",
-      alternativ_tekst: "Bilde av Puudklubbens stue"
-    },
-    {
-      kilde: "../img/hyttebilder/puudklubb/puudklubb_sov.jpg",
-      alternativ_tekst: "Bilde av Puudklubbens soverom"
-    },
-    {
-      kilde: "../img/hyttebilder/puudklubb/puudklubb_peis.jpg",
-      alternativ_tekst: "Bilde av Puudklubbens peis"
-    },
-    {
-      kilde: "../img/hyttebilder/puudklubb/puudklubb.jpg",
-      alternativ_tekst: "Bilde av hytten Puudklubb"
-    }
-  ],
-
-  [ // 9
-    {
-      kilde: "../img/hyttebilder/sluggmøll/sluggmøll_ute.jpg",
-      alternativ_tekst: "Bilde av Sluggmølls uteområde"
-    },
-    {
-      kilde: "../img/hyttebilder/sluggmøll/sluggmøll_stue.jpg",
-      alternativ_tekst: "Bilde av Sluggmølls stue"
-    },
-    {
-      kilde: "../img/hyttebilder/sluggmøll/sluggmøll_sov.jpg",
-      alternativ_tekst: "Bilde av Sluggmølls soverom"
-    },
-    {
-      kilde: "../img/hyttebilder/sluggmøll/sluggmøll.jpg",
-      alternativ_tekst: "Bilde av hytten Sluggmøll"
-    }
-  ],
-
-  [ // 10
-    {
-      kilde: "../img/hyttebilder/tiulla/tiulla_stue.jpg",
-      alternativ_tekst: "Bilde av Tiullas stue"
-    },
-    {
-      kilde: "../img/hyttebilder/tiulla/tiulla_sov.jpg",
-      alternativ_tekst: "Bilde av Tiullas soverom"
-    },
-    {
-      kilde: "../img/hyttebilder/tiulla/tiulla_kjokken.jpg",
-      alternativ_tekst: "Bilde av Tiullas kjøkken"
-    },
-    {
-      kilde: "../img/hyttebilder/tiulla/tiulla.jpg",
-      alternativ_tekst: "Bilde av hytten Tiulla"
-    }
-  ]
-]
-
-//alternativ metode
 
 function slideshow_hytte_generator(slideshowNumber) {
-  console.log(slideshow_hytte[slideshowNumber].length)
   let slideshow_hytte_container = document.getElementById("slideshow_hytter")
   slideshow_hytte_container.innerHTML = "";
-  for (let slide of slideshow_hytte[slideshowNumber]) {
+  for (let slide of hytter[slideshowNumber].info.img) {
     let div = document.createElement("div");
     div.className = "mySlides fade";
 
@@ -269,7 +54,7 @@ function slideshow_hytte_generator(slideshowNumber) {
 
     let buttons = document.getElementById("slideshow_buttons");
     buttons.innerHTML = "";
-    for (let i = 1; i < slideshow_hytte[slideshowNumber].length + 1; i++) {
+    for (let i = 1; i < hytter[slideshowNumber].info.img.length + 1; i++) {
       let span = document.createElement("span");
       span.className = "dot";
 
@@ -317,11 +102,11 @@ let SlideshowIntervalID = setInterval(plusSlides, 5000, 1);
 
 let slideIndex = 1;
 showSlides(slideIndex);
-/*
+
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
-*/
+
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
