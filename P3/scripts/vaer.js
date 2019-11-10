ba3a1b619f497b5e58e11a80938b922b86dd933f
Kimo <chaim0314@gmail.com>
2019-11-10 16:04:22 +0100

    vaerguden til aksjon

--- P3/scripts/vaer.js
+++ P3/scripts/vaer.js
@@ -32,14 +32,14 @@ function generer_vaer(seed) {
         "vekt": 1,
         "temp": [-50, 500]
     }];
-    console.log('vaers',vaers)
+    // console.log('vaers',vaers)
     let summen = 0;
     for (let i in vaers) {
         summen += vaers[i].vekt;
     }
 
     for (let i in vaers) {
-        console.log(`sum:${summen}|   floor(seed*sum): ${Math.floor(seed * summen)}|   vaervekt:${vaers[i].vekt}`)
+        // console.log(`sum:${summen}|   floor(seed*sum): ${Math.floor(seed * summen)}|   vaervekt:${vaers[i].vekt}`)
         if (summen - Math.floor(seed * summen) <= vaers[i].vekt) {
             vaers[i].temp = Math.floor(seed * (vaers[i].temp[1]) + vaers[i].temp[0])
             return vaers[i];
